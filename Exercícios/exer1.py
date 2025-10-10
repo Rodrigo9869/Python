d7da3a5fdb62b87dbaf6636170ac5be4b9544b1f
Rodrigo Pereira <rodrigo.pereira.t0129869@edu.atec.pt>
2025-10-10 08:03:56 +0100

    conversão de segundos para horas, minutos e segundos

--- Exercícios/exer1.py
+++ Exercícios/exer1.py
@@ -1,6 +1,10 @@
 segundos = int(input("Diga um numero para converter de segundos para horas,minutos e segundos: "))
 horas = segundos // 3600
-minutos = horas 
+restante = segundos % 3600
+minutos = restante // 60
+segundo = restante % 60
+
+print(f"O número fica em {horas} hora(s),{minutos} minutos e {segundo} segundos")
 
 
 
